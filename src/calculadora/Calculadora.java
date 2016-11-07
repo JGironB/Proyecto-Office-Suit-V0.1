cd8d0e7c14c17be331f513b53651928858b61d71
Juan Pablo Girón Bastidas <juangironb@gmail.com>
2016-11-07 12:24:11 -0500

    Creación de lógica de la calculadora

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,10 +1,39 @@
 package calculadora;
 
-public class Calculadora {
+import java.io.*;
+import java.util.GregorianCalendar;
+import javax.swing.SwingUtilities;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Calculadora 
+{
 
-	}
+	public static void main(String[] args) 
+	{
+		GregorianCalendar calendar = new GregorianCalendar();
+		
+		try 
+		{
+			FileWriter writing = new FileWriter("C:\\Historial.txt", true);
+			BufferedWriter bw = new BufferedWriter(writing);
+			PrintWriter pw = new PrintWriter(bw);
+			
+			bw.write(calendar.getTime()+"");
+			bw.newLine();
+				
+			bw.close();
+			pw.close();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+		InterfazUsuario ui = new InterfazUsuario();
+        SwingUtilities.invokeLater(ui);
+    }
 
 }
+
+
+
+
